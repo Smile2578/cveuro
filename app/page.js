2ba01b0f29a0c3623d66db0a3e7d007edebaa030
Smile2578 <kravcss@gmail.com>
2024-09-26 13:25:14 +0200

    bug scroll2

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { 
   Button, 
@@ -19,7 +18,6 @@ import {
   Star, 
   Timeline 
 } from '@mui/icons-material';
-import { motion, AnimatePresence } from 'framer-motion';
 import NavBar from './components/common/NavBar';
 import Footer from './components/common/Footer';
 import theme from './theme';
@@ -27,43 +25,19 @@ import theme from './theme';
 export default function LandingPage() {
   const router = useRouter();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const [hoveredCard, setHoveredCard] = useState(null);
-
-  const cardVariants = {
-    offscreen: { y: 50, opacity: 0 },
-    onscreen: { 
-      y: 0, 
-      opacity: 1,
-      transition: { type: 'spring', bounce: 0.4, duration: 0.8 }
-    }
-  };
-
-  const iconVariants = isMobile ? {} : {
-    rotate: {
-      rotate: 360,
-      transition: { duration: 0.5, ease: 'easeInOut' }
-    },
-    stop: {
-      rotate: 0,
-      transition: { duration: 0.5, ease: 'easeInOut' }
-    }
-  };
-
-  const contentVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { 
-      opacity: 1, 
-      y: 0,
-      transition: {
-        duration: 0.5,
-        ease: "easeOut"
-      }
-    }
-  };
 
   const handleCreateCV = () => {
     router.push('/cvgen');
   };
+
+  const steps = [
+    { icon: Person, title: "Informations personnelles", description: "Saisissez vos coordonnées et présentez-vous en quelques mots." },
+    { icon: School, title: "Formation", description: "Mettez en avant votre parcours académique et vos diplômes." },
+    { icon: Work, title: "Expérience professionnelle", description: "Détaillez vos expériences et réalisations professionnelles." },
+    { icon: Language, title: "Compétences linguistiques", description: "Indiquez les langues que vous maîtrisez et votre niveau." },
+    { icon: Star, title: "Compétences techniques", description: "Listez vos compétences techniques et professionnelles." },
+    { icon: Timeline, title: "Projets et réalisations", description: "Présentez vos projets marquants et vos accomplissements." },
+  ];
 
   return (
     <Box sx={{
@@ -73,140 +47,87 @@ export default function LandingPage() {
       backgroundColor: theme.palette.background.default,
     }}>
       <NavBar />
-      <Box sx={{
+      <Container component="main" maxWidth="lg" sx={{ 
+        py: { xs: 4, sm: 6 },
         flexGrow: 1,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        overflowY: 'auto',
-        WebkitOverflowScrolling: 'touch', // Améliore le défilement sur iOS
       }}>
-        <AnimatePresence mode="wait">
-          <motion.div
-            key="landing-content"
-            variants={contentVariants}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              flexGrow: 1,
+        <Typography variant="h3" sx={{ 
+          color: theme.palette.primary.dark,
+          fontSize: { xs: '2rem', sm: '2.5rem' },
+          textAlign: 'center',
+          fontWeight: 'bold',
+          mb: 3,
+          mt: isMobile ? 5 : 2,
+        }}>
+          Créez, personnalisez et gérez votre CV en toute simplicité. 
+        </Typography>
+        <Typography variant="h6" sx={{ 
+          mb: 5, 
+          textAlign: 'center', 
+          color: theme.palette.text.primary,
+          fontSize: { xs: '1rem', sm: '1.2rem' },
+          maxWidth: '700px',
+          lineHeight: 1.6,
+          mx: 'auto',
+        }}>
+          Notre outil intuitif vous guide à chaque étape pour mettre en valeur vos compétences et expériences.
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mb: 5 }}>
+          <Button 
+            variant="contained" 
+            size="large" 
+            color="primary"
+            onClick={handleCreateCV}
+            sx={{
+              padding: '10px 25px',
+              fontSize: '1rem',
+              borderRadius: '25px',
+              boxShadow: '0 4px 6px rgba(50, 50, 93, 0.11)',
+              '&:hover': {
+                boxShadow: '0 7px 14px rgba(50, 50, 93, 0.1)',
+              },
             }}
           >
-            <Container maxWidth="lg" sx={{ 
-              py: { xs: 4, sm: 6 },
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexGrow: 1,
-            }}>
-              <Typography variant="h3" sx={{ 
-                color: theme.palette.primary.dark,
-                fontSize: { xs: '2rem', sm: '2.5rem' },
-                textAlign: 'center',
-                fontWeight: 'bold',
-                mb: 3,
-                mt: isMobile ? 5 : 2,
+            Créer mon CV maintenant
+          </Button>
+        </Box>
+        <Grid container spacing={4} justifyContent="center">
+          {steps.map((step, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card sx={{ 
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                borderRadius: '15px',
+                boxShadow: '0 10px 20px rgba(0,0,0,0.1)',
+                transition: 'box-shadow 0.3s ease-in-out',
+                '&:hover': {
+                  boxShadow: '0 15px 30px rgba(0,0,0,0.15)',
+                }
               }}>
-                Créez, personnalisez et gérez votre CV en toute simplicité. 
-              </Typography>
-              <Typography variant="h6" sx={{ 
-                mb: 5, 
-                textAlign: 'center', 
-                color: theme.palette.text.primary,
-                fontSize: { xs: '1rem', sm: '1.2rem' },
-                maxWidth: '700px',
-                lineHeight: 1.6
-              }}>
-                Notre outil intuitif vous guide à chaque étape pour mettre en valeur vos compétences et expériences.
-              </Typography>
-              <motion.div
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <Button 
-                  variant="contained" 
-                  size="large" 
-                  color="primary"
-                  onClick={handleCreateCV}
-                  sx={{
-                    padding: '10px 25px',
-                    fontSize: '1rem',
-                    borderRadius: '25px',
-                    boxShadow: '0 4px 6px rgba(50, 50, 93, 0.11)',
-                    transition: 'all 0.3s',
-                    '&:hover': {
-                      transform: 'translateY(-2px)',
-                      boxShadow: '0 7px 14px rgba(50, 50, 93, 0.1)',
-                    },
-                    '&:active': { transform: 'translateY(1px)' }
-                  }}
-                >
-                  Créer mon CV maintenant
-                </Button>
-              </motion.div>
-              <Grid container spacing={4} justifyContent="center" sx={{ mt: 5 }}>
-                {[
-                  { icon: Person, title: "Informations personnelles", description: "Saisissez vos coordonnées et présentez-vous en quelques mots." },
-                  { icon: School, title: "Formation", description: "Mettez en avant votre parcours académique et vos diplômes." },
-                  { icon: Work, title: "Expérience professionnelle", description: "Détaillez vos expériences et réalisations professionnelles." },
-                  { icon: Language, title: "Compétences linguistiques", description: "Indiquez les langues que vous maîtrisez et votre niveau." },
-                  { icon: Star, title: "Compétences techniques", description: "Listez vos compétences techniques et professionnelles." },
-                  { icon: Timeline, title: "Projets et réalisations", description: "Présentez vos projets marquants et vos accomplissements." },
-                ].map((step, index) => (
-                  <Grid item xs={12} sm={6} md={4} key={index}>
-                    <motion.div 
-                      variants={cardVariants} 
-                      initial="offscreen" 
-                      whileInView="onscreen" 
-                      viewport={{ once: true, margin: "-50px" }}
-                      whileHover={isMobile ? {} : { scale: 1.05 }}
-                      whileTap={isMobile ? {} : { scale: 0.95 }}
-                      onHoverStart={() => !isMobile && setHoveredCard(index)}
-                      onHoverEnd={() => !isMobile && setHoveredCard(null)}
-                    >
-                      <Card sx={{ 
-                        height: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        borderRadius: '15px',
-                        boxShadow: '0 10px 20px rgba(0,0,0,0.1)',
-                        transition: 'all 0.3s ease-in-out',
-                        '&:hover': {
-                          boxShadow: '0 15px 30px rgba(0,0,0,0.15)',
-                        }
-                      }}>
-                        <CardContent sx={{ 
-                          display: 'flex', 
-                          flexDirection: 'column', 
-                          alignItems: 'center',
-                          padding: '1.5rem'
-                        }}>
-                          <motion.div
-                            variants={iconVariants}
-                            animate={!isMobile && hoveredCard === index ? 'rotate' : 'stop'}
-                          >
-                            <step.icon sx={{ fontSize: 50, color: theme.palette.primary.main, mb: 2 }} />
-                          </motion.div>
-                          <Typography variant="h6" align="center" sx={{ fontWeight: 'bold', mb: 1 }}>
-                            {step.title}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary" align="center">
-                            {step.description}
-                          </Typography>
-                        </CardContent>
-                      </Card>
-                    </motion.div>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </motion.div>
-        </AnimatePresence>
-      </Box>
+                <CardContent sx={{ 
+                  display: 'flex', 
+                  flexDirection: 'column', 
+                  alignItems: 'center',
+                  padding: '1.5rem'
+                }}>
+                  <step.icon sx={{ fontSize: 50, color: theme.palette.primary.main, mb: 2 }} />
+                  <Typography variant="h6" align="center" sx={{ fontWeight: 'bold', mb: 1 }}>
+                    {step.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" align="center">
+                    {step.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
       <Footer />
     </Box>
   );
