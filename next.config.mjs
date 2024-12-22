73f4c2cd7991e153fcd14fd79dcf23e072675c18
Smile2578 <kravcss@gmail.com>
2024-12-22 13:12:19 +0100

    nginx

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./app/i18n.js');
 
-const config = {};
+const config = {
+    headers: async () => [
+        {
+            source: '/:path*',
+            headers: [
+                {
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff',
+                },
+                {
+                    key: 'X-Frame-Options',
+                    value: 'DENY',
+                },
+                {
+                    key: 'X-XSS-Protection',
+                    value: '1; mode=block',
+                },
+            ],
+        },
+    ],
+};
 
 export default withNextIntl(config);
